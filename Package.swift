171b9734b044bf057f460624e4dd263b13acc98e
JChoy <jchoyeclipse@gmail.com>
2026-05-04 21:55:20 +0000

    chore: update Package.swift for v0.1.3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZeroTalkSDK",
-            url: "https://github.com/generativelab-develop/zerotalk-ios-sdk/releases/download/v0.1.2/ZeroTalkSDK.xcframework.zip",
-            checksum: "f5293f5bbb042f2785d04f5a4c2bcf04f518a88e816d5cd70307728f32c89eea"
+            url: "https://github.com/generativelab-develop/zerotalk-ios-sdk/releases/download/v0.1.3/ZeroTalkSDK.xcframework.zip",
+            checksum: "8b5dce51be99b6c2c20ab6a142d5719f39f4d905aab15ddb3c748cd412f1d30c"
         ),
         // SPM single binaryTarget visibility bug workaround
         .target(name: "_ZeroTalkSDKTarget"),
